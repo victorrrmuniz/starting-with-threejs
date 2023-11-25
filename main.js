5f753b05d72445be11621ddfea48911386144f40
Victor Muniz <victorrmunizz@gmail.com>
2023-11-25 01:21:55 -0300

    drawing a rotating cube with three.js

---
+++ main.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if (WebGL.isWebGLAvailable()) {
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	const geometry = new THREE.DodecahedronGeometry(2, 1);
+	const material = new THREE.MeshBasicMaterial( { color: 0x000ff } );
+	const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+	function animate() {
+		requestAnimationFrame( animate );
+
+		cube.rotation.x += 0.004;
+		cube.rotation.y += 0.004;
+
+		renderer.render( scene, camera );
+	}
+
+	animate();
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById('container').appendChild(warning);
+}
+
